a65ce6dbc3c9520a997980fcdff977d6a7d489e3
jequinterop <jo.quintero@duocuc.cl>
2024-06-05 23:31:58 -0400

    agregar vistas y urls

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -1,8 +1,15 @@
 #from django.conf.urls import url
 
-from django.urls import path
+from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('index', views.index, name='index'),
+    path('listadoSQL', views.listadoSQL,name='listadoSQL'),
+    path('listado_generos',views.lista_generos,name='lista_generos'),
+    path('crud',views.crud,name='crud'),
+    path('alumnosAdd',views.alumno_agregar,name='alumnoAdd'),
 ]
+
+#La funcion PATH en su estructura separa los parametros por comas ()
--- alumnos/views.py
+++ alumnos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 from .models import Alumno, Genero
 
 # Create your views here.
@@ -7,5 +7,21 @@ def index(request):
     alumnos=Alumno.objects.all()
     context={"alumnos":alumnos}
     return render(request, 'alumnos/index.html', context)
+
+def listadoSQL(request):
+    alumnos = Alumno.objects.raw("SELECT * FROM alumnos_alumno")
+    print(alumnos)
+    context = {"alumnos":alumnos}
+    return render(request, 'alumnos/listadoSQL.html', context)
+
+def lista_generos(request):
+    generos = Genero.objects.all()
+    context = {"generos":generos}
+    return render(request,'alumnos/genero.html',context)
+
+def crud(request):
+    alumnos =Alumno.objects.all()
+    context = {"alumnos":alumnos}
+    return render(request,'alumnos/alumnos_list.html',context)
 
 
